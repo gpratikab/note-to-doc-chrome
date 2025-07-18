dd6129dea35ca65ba8644fed9ebebb0a678fe265
gpratikab <pratikab@google.com>
2025-07-18 17:59:42 +0530

    fix issues

--- content.js
+++ content.js
@@ -251,13 +251,8 @@
         checklistBtn.innerHTML = `<svg viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>`;
         checklistBtn.addEventListener('mousedown', (e) => {
             e.preventDefault();
-            const item = document.createElement('div');
-            item.className = 'qn-checklist-item';
-            item.setAttribute('data-checked', 'false');
-            item.contentEditable = 'true';
-            item.innerHTML = '&#8203;';
             editor.focus();
-            document.execCommand('insertHTML', false, item.outerHTML + '<div>&#8203;</div>');
+            document.execCommand('insertHTML', false, '<div class="qn-checklist-item" data-checked="false" contenteditable="true">&#8203;</div>');
         });
         toolbar.appendChild(checklistBtn);
         
@@ -301,7 +296,7 @@
             debouncedCacheUpdate();
         }, 50));
 
-        ui.host.addEventListener('click', (e) => {
+        ui.editor.addEventListener('click', (e) => {
             if (e.target.classList.contains('qn-checklist-item')) {
                  const isChecked = e.target.getAttribute('data-checked') === 'true';
                  e.target.setAttribute('data-checked', !isChecked);
@@ -400,3 +395,4 @@
         };
     }
 })();
+
